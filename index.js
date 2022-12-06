2d167ff00bed6916fe15f68c46d67d393166eac8
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-12-06 17:26:53 -0500

    added inquirer prompts for engineer and intern

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Manager = require('./lib/Manager')
 const teamRoster = [];
 
 const promptManager = () => {
-    return inquirer.prompt ([
+    return inquirer.prompt([
         {
             type: 'input',
             message: `What is the Managers's name?`,
@@ -30,44 +30,99 @@ const promptManager = () => {
             name: 'officeNumber',
         },
     ])
-    .then(answers => {
-        console.log(answers);
-        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        .then(response => {
+            console.log(response);
+            const manager = new Manager(response.name, response.id, response.email, response.officeNumber)
             teamRoster.push(manager);
-            promptMenu();
-    })
+            promptSelection();
+        })
 };
 
+const promptSelection = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: `Do you want to add a team member?`,
+            name: 'selection',
+            choices: ['Engineer', 'Intern', 'I do not have any other team members to add']
+        },
+    ])
+        .then(choice => {
+            switch (choice.selection) {
+                case "Engineer":
+                    promptEngineer();
+                    break;
+                case "Intern":
+                    promptIntern();
+                    break;
+                default:
+                    buildTeam();
+            }
+        });
+};
 
-// inquirer
-//     .prompt([
-//         {
-//             type: 'input',
-//             message: `What is the employee's name?`,
-//             name: 'name',
-//         },
-//         {
-//             type: 'input',
-//             message: `What is the employee's id?`,
-//             name: 'id',
-//         },
-//         {
-//             type: 'input',
-//             message: `What is the employee's email?`,
-//             name: 'email',
-//         },
-//         {
-//             type: 'input',
-//             message: `What is the Manager's office number?`,
-//             name: 'officeNumber',
-//         },
-//         {
-//             type: 'list',
-//             message: `Do you want to add another team member?`,
-//             name: 'associate',
-//             choices: ['Engineer', 'Intern', 'I do not have any other team members to add']
-//         },
-//     ])
+const promptEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: `What is the Engineer's name?`,
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: `What is the Engineer's id?`,
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: `What is the Engineer's email?`,
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: `What is the Engineer's GitHub username?`,
+            name: 'github',
+        },
+    ])
+        .then(response => {
+            console.log(response);
+            const engineer = new Engineer(response.name, response.id, response.email, response.github)
+            teamRoster.push(engineer);
+            promptSelection();
+        })
+};
+
+const promptIntern = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: `What is the Intern's name?`,
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: `What is the Intern's id?`,
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: `What is the Intern's email?`,
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: `What school did the Intern attend?`,
+            name: 'school',
+        },
+    ])
+        .then(response => {
+            console.log(response);
+            const intern = new Intern(response.name, response.id, response.email, response.school)
+            teamRoster.push(intern);
+            promptSelection();
+        })
+};
+
 
     // .then((response) => {
     //     let EmployeeText = Employee(response);
