911efbb795c99c6af2cb69ae6c109d1e6a4cd026
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-12-07 09:10:48 -0500

    aded dynmaic email and github links, updated font size on header

--- generatePage.js
+++ generatePage.js
@@ -11,7 +11,7 @@ const generateRoster = (team) => {
 <div class="header bg-primary">Manager</div>
 <div class="card-body">
     <p class="card-body">Manager ID: ${manager.id}</p>
-    <p class="card-body">Email: ${manager.email}</p>
+    <p class="card-body">Email: <a href="mailto:email@example.com">${manager.email}</a></p>
     <p class="card-body">Office Number: ${manager.officeNumber}</p>
 </div>
 </div>
@@ -28,8 +28,8 @@ const generateRoster = (team) => {
 <div class="header bg-primary">Engineer</div>
 <div class="card-body">
     <p class="card-body">Engineer ID: ${engineer.id}</p>
-    <p class="card-body">Email: ${engineer.email}</p>
-    <p class="card-body">GitHub: ${engineer.github}</p>
+    <p class="card-body">Email: <a href="mailto:email@example.com">${engineer.email}</a></p>
+    <p class="card-body">GitHub: <a href="https://www.github.com/${engineer.github}" target="_blank">${engineer.github}</a></p>
 </div>
 </div>
     `
@@ -45,7 +45,7 @@ const generateRoster = (team) => {
 <div class="header bg-primary">Intern</div>
 <div class="card-body">
     <p class="card-body">Intern ID: ${intern.id}</p>
-    <p class="card-body">Email: ${intern.email}</p>
+    <p class="card-body">Email: <a href="mailto:email@example.com">${intern.email}</a></p>
     <p class="card-body">School: ${intern.school}</p>
 </div>
 </div>
